2c94eb714c1c2ccc400a80182c07c0451343cc74
Michael Equi <equim20@student.jhs.net>
2018-11-04 21:29:33 -0800

    Got simulation working with teleop

--- src/racecar_control/src/keyboard_teleop.py
+++ src/racecar_control/src/keyboard_teleop.py
@@ -37,7 +37,7 @@ def vels(speed,turn):
 
 if __name__=="__main__":
   settings = termios.tcgetattr(sys.stdin)
-  pub = rospy.Publisher('/vesc/ackermann_cmd_mux/input/teleop', AckermannDriveStamped)
+  pub = rospy.Publisher('/vesc/low_level/ackermann_cmd_mux/output', AckermannDriveStamped)
   rospy.init_node('keyop')
 
   x = 0
@@ -45,7 +45,7 @@ if __name__=="__main__":
   status = 0
 
   try:
-    while(1):
+    while not rospy.is_shutdown():
        key = getKey()
        if key in keyBindings.keys():
           x = keyBindings[key][0]
